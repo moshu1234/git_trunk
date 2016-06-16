bfeafe7562858a8761602ba9765c1da56b27143c
moshu1234 <187032677@163.com>
2016-06-16 10:31:54 +0800

    add few shape format files, and tested it on login button

--- FatBaby/app/src/main/java/com/example/andrewliu/fatbaby/UI/SlidMenu/Login.java
+++ FatBaby/app/src/main/java/com/example/andrewliu/fatbaby/UI/SlidMenu/Login.java
@@ -9,6 +9,7 @@ import android.support.v4.app.Fragment;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -62,7 +63,7 @@ public class Login extends Fragment {
     }
 
     public void login(){
-        Button b_login=(Button)view.findViewById(R.id.login_b);
+        final Button b_login=(Button)view.findViewById(R.id.login_b);
         b_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -74,6 +75,19 @@ public class Login extends Fragment {
 //                }
 //                loginHandler.sendMessage(message);
                 userCompare();
+            }
+        });
+        b_login.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+//                Log.e("onTouch","action:"+event.getAction());
+                if(event.getAction() == MotionEvent.ACTION_UP || event.getAction() == MotionEvent.ACTION_CANCEL){
+                    b_login.setBackgroundResource(R.drawable.shape_button_background);
+                }
+                else{
+                    b_login.setBackgroundResource(R.drawable.shape_button_press);
+                }
+                return false;
             }
         });
     }
